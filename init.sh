d187549b8ecc7e2544f3b6d48ed6d2506873a9ab
KyQiao <kqiao@connect.ust.hk>
2020-12-07 18:23:48 +0800

    init file

---
+++ init.sh
@@ -0,0 +1,5 @@
+# this will install binary in ./bin folder
+curl -sfL https://git.io/chezmoi | sh
+# git remote add origin https://github.com/KyQiao/dotfiles.git
+chezmoi init https://github.com/KyQiao/dotfiles.git
+
