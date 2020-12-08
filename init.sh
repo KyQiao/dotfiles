93a38ea86a550aa99f9f802607ee74f722cb13a9
KyQiao <kqiao@connect.ust.hk>
2020-12-08 07:44:08 -0500

    fix path

--- init.sh
+++ init.sh
@@ -1,11 +1,16 @@
 # this will install binary in ./bin folder
-cd ~ && mkdir -p usr
+pushd ~
+mkdir -p usr
 mkdir -p backup && mkdir -p code && mkdir -p data
-cd usr && mkdir -p bin && mkdir -p lib mkdir -p include
+pushd ~/usr
+mkdir -p bin && mkdir -p lib mkdir -p include
 curl -sfL https://git.io/chezmoi | sh
 # git remote add origin https://github.com/KyQiao/dotfiles.git
 ./bin/chezmoi init https://github.com/KyQiao/dotfiles.git
 ./bin/chezmoi apply
+
+popd
+popd
 
 gitV="$(git --version 2>&1)"
 re="git version ([0-9])"
