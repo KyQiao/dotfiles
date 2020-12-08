46eb801d705138176c5ca165f2c2e46cbf9f91eb
KyQiao <kqiao@connect.ust.hk>
2020-12-08 07:40:20 -0500

    add git init bash script

--- init.sh
+++ init.sh
@@ -6,3 +6,16 @@ curl -sfL https://git.io/chezmoi | sh
 # git remote add origin https://github.com/KyQiao/dotfiles.git
 ./bin/chezmoi init https://github.com/KyQiao/dotfiles.git
 ./bin/chezmoi apply
+
+gitV="$(git --version 2>&1)"
+re="git version ([0-9])"
+if [[ $gitV =~ $re ]];then
+	if [[ ${BASH_REMATCH[1]} -ge 2 ]];then
+		echo "git version >= 2"
+	else
+		bash ~/.local/sh/git.sh
+	fi  
+else
+	echo "REGEX dosen't match"
+fi
+
