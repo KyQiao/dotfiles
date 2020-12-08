f909d509ae8724d5eef08d6fabe5adbb59597798
KyQiao <kqiao@connect.ust.hk>
2020-12-08 19:02:12 +0800

    fixed git.sh

--- private_dot_local/sh/git.sh
+++ private_dot_local/sh/git.sh
@@ -1,9 +1,10 @@
-cd ~/backup
-wget https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.29.2.tar.xz
-extract git-2.29.2.tar.gz
-cd git-2.29.2/
+pushd ~/backup
+wget -N -nv https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.29.2.tar.gz
+tar -xzf git-2.29.2.tar.gz
+pushd git-2.29.2/
 ./configure --prefix=$HOME/usr
-make -j 8 && make insatll
-
+make -j 8 && make install
+popd
+popd
 # For the init env, linking curl lib should works fine
 # If curl lib is buggy, change the lib and bin to local one
